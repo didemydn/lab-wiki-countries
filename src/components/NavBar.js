b10c367f6e7d1df26f3bd62c7032a2ee42fbdda9
Didem Cakir <didemydn@gmail.com>
2023-08-11 13:54:58 +0200

    done

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+
+function NavBar (){
+    return(
+
+        <nav className="navbar bg-primary">
+    <span className="navbar-brand"> WikiCountries</span>
+    </nav>
+    )
+}
+export default NavBar
